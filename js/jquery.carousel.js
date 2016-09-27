97a4aa09ce4f2be84acd0514921cdbf367d14d13
vinosgray <komarichev@gmail.com>
2016-09-27 15:08:05 +0300

    rebuild carousel

--- js/jquery.carousel.js
+++ js/jquery.carousel.js
@@ -5,26 +5,25 @@
     var rightUIEl = $('.crousel-arrow-right');
     var pixelOffset = 325;
     var currentValue = 0;
-    
+
     var elementCount = elementList.find("li").length;
     var minimumOffset = -((elementCount - 5) * pixelOffset);
     var maximumOffset = 0;
     leftUIEl.click(function() {
          if (currentValue != maximumOffset) {
         currentValue += pixelOffset;
-        elementList.animate({left: currentValue + "px"}, 500);
+        elementList.animate({marginLeft: currentValue + "px"}, 500);
     }
     });
- 
+
     rightUIEl.click(function() {
         if (currentValue != minimumOffset) {
         currentValue -= pixelOffset;
-        elementList.animate({left: currentValue + "px"}, 500);
+        elementList.animate({marginLeft: currentValue + "px"}, 500);
     }
     });
         return this;
-    
+
     };
 
 })(jQuery);
-
