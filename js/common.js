a06efb61c0b3b7d55bf692a7a1616a9916e7178f
vinosgray <komarichev@gmail.com>
2016-09-28 11:48:05 +0300

    finish

--- js/common.js
+++ js/common.js
@@ -7,7 +7,7 @@ var user={
 	img:"https://i.imgur.com/RFk9hgO.gif",
 	prof:"Студент заборотроительного техникума",
 	wishes:[ "заборы строить не интересно","Платят мало", "Приходится работать ночью"],
-	phone:"+380672222222",
+	phone:"+38(067) 222-22-22",
 	society:["vk.com","ok.ru"],
 	feedback:"Если нужно, могу построить вам забор"
 };
@@ -17,10 +17,15 @@ var user2={
 	prof:"Аналитик товарного рынка",
 	wishes:[ "Создавать магию - это мечта детства.","Изучаю профессию, которая сделает меня космополитом.", "Процесс изучения frontend(IT) процесс бесконечный."],
 	phone:"+38 (067) 625-18-58",
+	fb:"https://www.facebook.com/vinosgray",
 	society:["vk.com","ok.ru"],
 	feedback:"Если нужно, могу провести аналитику игристых вин по предприятиям отрасли."
 };
+//---------------------------------------
 var templated = tmpl($('#tmpl').html());
-var result = templated(user2);
+var result = templated(user);
 $('#ejohn-box').append(result);
-//
+//---------------------------------------
+var templated2 = _.template($('#tmpl2').html());
+var result2 = templated2();
+$('#lodash-box').append(result2);
